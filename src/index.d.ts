cf9b5ab6f5522863e51457f6cecb57299e9be720
leny <info@flatland.be>
2019-04-14 17:42:36 +0200

    Add typescript types declaration file
    cf. #1

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+export declare const useLocalStorage: <V>(key: string, defaultValue: V) => [V, (V) => void];
+
+export declare const useSessionStorage: <V>(key: string, defaultValue: V) => [V, (V) => void];
